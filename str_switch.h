d867f2aca168a4391bfd87d2f553b4a93908e576
Igor Sadchenko <igor.sadchenko@gmail.com>
2019-07-22 18:01:53 +0300

    Add hash opertor

--- str_switch.h
+++ str_switch.h
@@ -1,51 +1,61 @@
 ﻿#ifndef STR_SWITCH_H
 #define STR_SWITCH_H
 
+#include <cstdint>
+#include <cassert>
 #include <string>
 
-#define SWITCH(str)  switch(s_s::str_hash_for_switch(str))
-#define CASE(str)    static_assert(s_s::str_is_correct(str) && (s_s::str_len(str) <= s_s::MAX_LEN),\
-"CASE string contains wrong characters, or its length is greater than 9");\
-case s_s::str_hash(str, s_s::str_len(str))
+
+#define SWITCH(str)  switch(utils::string_switch::str_hash_for_switch(str))
+#define CASE(str)    static_assert(utils::string_switch::str_is_correct(str) && (utils::string_switch::str_len(str) <= utils::string_switch::MAX_LEN),\
+"CASE string contains wrong characters, or its length is greater than MAX_LEN");\
+case utils::string_switch::str_hash(str, utils::string_switch::str_len(str))
 #define DEFAULT  default
 
-namespace s_s
-{
-    typedef unsigned char uchar;
-    typedef unsigned long long ullong;
 
-    const uchar MAX_LEN = 9;
-    const ullong N_HASH = static_cast<ullong>(-1);
+namespace utils {
+namespace string_switch {
 
-    constexpr ullong raise_128_to(const uchar power)
+    using uchar = uint8_t;
+    using ullong = uint64_t;
+
+    constexpr size_t MAX_LEN = 64;
+    constexpr ullong N_HASH = static_cast<ullong>(-1);
+
+    constexpr ullong raise_128_to(const size_t power)
     {
-        return 1ULL << 7 * power;
+        return 1ULL << (7 * (power % 7));
     }
 
-    constexpr bool str_is_correct(const char* const str)
+    constexpr bool str_is_correct(const char*  const str)
     {
         return (static_cast<signed char>(*str) > 0) ? str_is_correct(str + 1) : (*str ? false : true);
     }
 
-    constexpr uchar str_len(const char* const str)
+    constexpr size_t str_len(const char*  str)
     {
         return *str ? (1 + str_len(str + 1)) : 0;
     }
 
-    constexpr ullong str_hash(const char* const str, const uchar current_len)
+    constexpr ullong str_hash(const char*  str, const size_t current_len)
     {
         return *str ? (raise_128_to(current_len - 1) * static_cast<uchar>(*str) + str_hash(str + 1, current_len - 1)) : 0;
     }
 
-    inline ullong str_hash_for_switch(const char* const str)
+    constexpr ullong str_hash_for_switch(const char*  str)
     {
         return (str_is_correct(str) && (str_len(str) <= MAX_LEN)) ? str_hash(str, str_len(str)) : N_HASH;
     }
-
-    inline ullong str_hash_for_switch(const std::string& str)
-    {
-        return (str_is_correct(str.c_str()) && (str.length() <= MAX_LEN)) ? str_hash(str.c_str(), str.length()) : N_HASH;
-    }
 }
+}
+
+
+constexpr typename utils::string_switch::ullong operator "" _hash(char const* const str, size_t len)
+{
+    assert(utils::string_switch::str_is_correct(str) && (len <= utils::string_switch::MAX_LEN) && 
+        "CASE string contains wrong characters, or its length is greater than MAX_LEN");
+    return utils::string_switch::str_hash(str, len);
+}
+
 
 #endif  // STR_SWITCH_H
